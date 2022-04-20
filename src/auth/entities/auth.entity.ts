760417deadb5ec1ebf2cfbe8b43dc4aad212140c
DHN Chandan <php.chandan@gmail.com>
2022-04-21 00:16:06 +0600

    Add auth dto

--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,1 +1,14 @@
-export class Auth {}
+import { Exclude } from 'class-transformer';
+import { BaseEntity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Auth extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @Column({ type: 'varchar' })
+  public email!: string;
+
+  @Exclude()
+  @Column({ type: 'varchar' })
+  public password!: string;
+}
