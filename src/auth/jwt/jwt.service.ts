970d9c86845a4d58a20fc66dc2807eade9f06caa
DHN Chandan <php.chandan@gmail.com>
2022-04-21 01:07:15 +0600

    Add jwt passport

--- src/auth/jwt/jwt.service.ts
+++ src/auth/jwt/jwt.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService as Jwt } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Auth } from '../entities/auth.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class JwtService {}
+export class JwtService {
+  constructor(
+    @InjectRepository(Auth)
+    private readonly authRepository: Repository<Auth>,
+    private readonly jwt: Jwt,
+  ) {}
+
+  public async decode(token: string): Promise<unknown> {
+    return this.jwt.decode(token);
+  }
+
+  public async validateUser(decoded: any): Promise<Auth> {
+    return this.authRepository.findOne(decoded.id);
+  }
+
+  public generateToken(auth: Auth): string {
+    return this.jwt.sign({ id: auth.id, email: auth.email });
+  }
+
+  public isPasswordValid(password: string, userPassword: string): boolean {
+    return bcrypt.compareSync(password, userPassword);
+  }
+
+  public encodePassword(password: string): string {
+    const salt: string = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(password, salt);
+  }
+
+  public async verifyToken(token: string): Promise<any> {
+    try {
+      return this.jwt.verify(token);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -1,1 +1,22 @@
-export class JwtStrategy {}
+import { Inject } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtService } from './jwt.service';
+import { Auth } from '../entities/auth.entity';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @Inject(JwtService)
+    private readonly jwtService: JwtService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: true,
+      secretOrKey: 'secret',
+    });
+  }
+
+  private validate(token: string): Promise<Auth | never> {
+    return this.jwtService.validateUser(token);
+  }
+}
