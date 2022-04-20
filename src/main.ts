a2b73349b883ba9b9245a35098155163e480a256
DHN Chandan <php.chandan@gmail.com>
2022-04-21 05:09:09 +0600

    Set microservice app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { INestMicroservice, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './auth/filter/http-exception.filter';
+import { protobufPackage } from './auth/auth.pb';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: INestMicroservice = await NestFactory.createMicroservice(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        url: '0.0.0.0:50051',
+        package: protobufPackage,
+        protoPath: join('node_modules/nest_micro_proto/proto/auth.proto'),
+      },
+    },
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  await app.listen();
 }
 bootstrap();
