890f183aca6539be52003727280a6f4ac2080ec5
DHN Chandan <php.chandan@gmail.com>
2022-04-23 14:36:45 +0600

    Config typeorm from configuration file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,26 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormAsyncConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      database: 'nest_micro_auth',
-      username: 'postgres',
-      password: '10091986',
-      entities: ['dist/**/*.entity.{ts,js}'],
-      synchronize: true, // never true in production!
-      // migrations: ['dist/src/db/migrations/*.js'],
-      // migrationsRun: true,
-      // cli: {
-      //   migrationsDir: 'src/db/migrations',
-      // },
-    }),
-    AuthModule,
-  ],
+  imports: [TypeOrmModule.forRootAsync(typeormAsyncConfig), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
